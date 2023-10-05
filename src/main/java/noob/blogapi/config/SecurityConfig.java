7cba902fb24d1f5d87ddead5613e5540977680ca
abdelmoneim <abdelmoneimelshafei@proton.me>
2023-10-05 14:30:21 +0300

    add inMemory Authentication and SecurityConfig

---
+++ src/main/java/noob/blogapi/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package noob.blogapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return  http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                   auth.requestMatchers(HttpMethod.GET,"/api/**").permitAll();
+                   auth.anyRequest().authenticated();
+                })
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails abdo = User.builder()
+                .username("abdo")
+                .password(passwordEncoder().encode("abdo"))
+                .roles("ADMIN")
+                .build();
+        UserDetails ahmed = User.builder()
+                .username("ahmed")
+                .password(passwordEncoder().encode("ahmed"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(abdo, ahmed);
+    }
+}
--- src/main/java/noob/blogapi/controller/PostController.java
+++ src/main/java/noob/blogapi/controller/PostController.java
@@ -9,6 +9,7 @@ import noob.blogapi.service.PostService;
 import noob.blogapi.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<PostDto>(postService.createPost(postDto), HttpStatus.CREATED);
     }
@@ -36,12 +38,15 @@ public class PostController {
     public ResponseEntity<PostDto> getById(@PathVariable Long id){
        return ResponseEntity.ok(postService.getById(id));
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<PostDto> updateById(@PathVariable Long id, @Valid @RequestBody PostDto postDto){
         PostDto postDto1 = postService.updateById(id , postDto);
         return new ResponseEntity<PostDto>(postDto1,HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteById(@PathVariable Long id){
         postService.deleteById(id);
